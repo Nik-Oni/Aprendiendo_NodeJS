25a0c63240f18f983ebb55ef0b3de723f0209722
[Nikion] <[nigutjim@gmail.com]>
2022-05-27 09:38:18 -0500

    escribir archivos, crear reemplazar y append

--- LeerArchivos/app.js
+++ LeerArchivos/app.js
@@ -1,5 +1,9 @@
 const fs = require('fs');
 
-fs.readdir('./archivos', { withFileTypes:true},(error,items)=>{
-    console.log(items)
+fs.appendFile('./archivos/texto-nuevo.txt', '\n   Nuevos datos',(error)=>{
+    if (error){
+        console.log('error al escribir archivo');
+    } else {
+        console.log('archivo modificado exitosamente');
+    }
 })
