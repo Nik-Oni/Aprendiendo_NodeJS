f865914ac24e82d9cadaba93cd1403a5a2f19777
[Nikion] <[nigutjim@gmail.com]>
2022-05-27 10:12:10 -0500

    copiar archivos a otra carpeta con metodos encadenados

--- LeerArchivos/app.js
+++ LeerArchivos/app.js
@@ -1,17 +1,20 @@
 const fs = require('fs');
 
-/* fs.copyFile('./archivos/archivo_original.txt', './archivos/archivo_nuevo.txt', (error)=>{
+fs.copyFile('./archivos/archivo_original.txt', './otros_archivos/archivo_original.txt', (error)=>{
     if (error) {
         console.log('error al copiar')
     } else {
+        fs.writeFile('./archivos/confirmacion.txt', 'archivo copiado Ok', (error)=>{
+            console.log('proceso finalizado')
+        })
         console.log('copia exitosa')
     }
-}) */
+})
 
-fs.unlink('./archivos/archivo_nuevo.txt', (error)=>{
+/* fs.unlink('./archivos/archivo_nuevo.txt', (error)=>{
     if (error) {
         console.log('error al eliminar')
     } else {
         console.log('eliminacion exitosa')
     }
-})
+}) */
