92eadaefa7506116e563418b9ffa1a8dcbc50d94
[Nikion] <[nigutjim@gmail.com]>
2022-05-27 09:50:15 -0500

    eliminacion permanente de archivos con node

--- LeerArchivos/app.js
+++ LeerArchivos/app.js
@@ -1,10 +1,17 @@
 const fs = require('fs');
 
-
-fs.copyFile('./archivos/archivo_original.txt', './archivos/archivo_nuevo.txt', (error)=>{
+/* fs.copyFile('./archivos/archivo_original.txt', './archivos/archivo_nuevo.txt', (error)=>{
     if (error) {
         console.log('error al copiar')
     } else {
         console.log('copia exitosa')
+    }
+}) */
+
+fs.unlink('./archivos/archivo_nuevo.txt', (error)=>{
+    if (error) {
+        console.log('error al eliminar')
+    } else {
+        console.log('eliminacion exitosa')
     }
 })
