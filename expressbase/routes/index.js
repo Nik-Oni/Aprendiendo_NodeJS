6b2ce65650ac0d1463a1bd086de0424a3087916d
[Nikion] <[nigutjim@gmail.com]>
2022-05-27 10:56:56 -0500

    Desplegar datos JSON en una plantilla con express generator

--- expressbase/routes/index.js
+++ expressbase/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
+const { fstat } = require('fs');
 var router = express.Router();
+const fs = require('fs');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  fs.readFile('./public/json/libros.json', (error, datos) => {
+    if (error) {
+      res.write ('error de lectura')
+    } else {
+      res.render('index', { datos: JSON.parse(datos)} );
+    }
+  })
 });
 
 module.exports = router;
