5fb90d5bd138dff5b0378e0c339be596c4b7d33c
[Nikion] <[nigutjim@gmail.com]>
2022-05-27 07:42:22 -0500

    primer commit creo

--- app.js
+++
@@ -1,9 +0,0 @@
-const http=require('http');
-
-const servidor =http.createServer( (peticion, respuesta)=>{
-    respuesta.writeHead(200, {'Content-type': 'text/html'});
-    respuesta.write('Respuesta para la direccion: ' + peticion.url)
-    console.log('peticion web');
-});
-servidor.listen(3000);
-console.log('Ejecutando servidor NodeJS');
