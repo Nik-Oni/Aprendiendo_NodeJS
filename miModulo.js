29dac9c01e32d818343095117a800f75cff764d9
[Nikion] <[nigutjim@gmail.com]>
2022-05-27 07:49:13 -0500

    todo bien

---
+++ miModulo.js
@@ -0,0 +1,10 @@
+const miModulo = {
+    multiplicar: (valor1) => {
+        console.log(valor1 * 3)
+    },
+    dividir: (valor1) => {
+        console.log(valor1 / 3)
+    },
+}
+
+module.exports = miModulo
